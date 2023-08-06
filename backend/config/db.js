58cd526f4f3192a6411a8006e9b4c2210b6dbda9
Salil Upadhyay <upsalil.01@gmail.com>
2023-08-06 11:08:21 +0530

    backend completed

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,10 +12,42 @@ const createTable = async () => {
   try {
     const connection = await mysql.createConnection(databaseConfig);
     const query = `
-       CREATE TABLE IF NOT EXISTS users(
-        name VARCHAR(50),
-        age INT
-       )
+    CREATE TABLE IF NOT EXISTS users (
+      gender VARCHAR(10),
+      title VARCHAR(10),
+      first_name VARCHAR(50),
+      last_name VARCHAR(50),
+      street_number INT,
+      street_name VARCHAR(100),
+      city VARCHAR(100),
+      state VARCHAR(100),
+      country VARCHAR(100),
+      postcode VARCHAR(20),
+      latitude DECIMAL(10, 7),
+      longitude DECIMAL(10, 7),
+      timezone_offset VARCHAR(10),
+      timezone_description VARCHAR(100),
+      uuid VARCHAR(100),
+      email VARCHAR(100),
+      username VARCHAR(50),
+      password VARCHAR(100),
+      salt VARCHAR(100),
+      md5 VARCHAR(100),
+      sha1 VARCHAR(100),
+      sha256 VARCHAR(100),
+      dob_date VARCHAR(100),
+      dob_age INT,
+      registered_date VARCHAR(100),
+      registered_age INT,
+      phone VARCHAR(20),
+      cell VARCHAR(20),
+      id_name VARCHAR(50),
+      id_value VARCHAR(50),
+      picture_large VARCHAR(255),
+      picture_medium VARCHAR(255),
+      picture_thumbnail VARCHAR(255),
+      nationality VARCHAR(10)
+      )
        `;
     await connection.query(query);
     connection.end();
--- backend/routes/User.route.js
+++ backend/routes/User.route.js
@@ -3,30 +3,162 @@ const express = require("express");
 const { databaseConfig } = require("../config/db");
 const userRouter = express.Router();
 
-// to post data and store in db
-userRouter.post("/", async (req, res) => {
-  const data = req.body.data;
+// to get the data from db
+
+userRouter.get("/", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const filter = req.query.country || null;
+
+  const offset = (page - 1) * limit; //calculating offset
   try {
     const connection = await mysql.createConnection(databaseConfig);
-    let batchSize = 3; // insert 3 rows in a single command
+
+    //query
+    let query = "SELECT SQL_CALC_FOUND_ROWS * FROM users";
+    if (filter) {
+      query += " WHERE country = ?"; //for filtering
+    }
+    query += " LIMIT ? OFFSET ?"; //paginate
+
+    if (filter) {
+      [results] = await connection.query(query, [filter, limit, offset]);
+      const [countResult] = await connection.query(
+        "SELECT FOUND_ROWS() as totalCount"
+      );
+      totalCount = countResult[0].totalCount;
+    } else {
+      [results] = await connection.query(query, [limit, offset]);
+      const [countResult] = await connection.query(
+        "SELECT FOUND_ROWS() as totalCount"
+      );
+      totalCount = countResult[0].totalCount;
+    }
+
+    // Closing connection
+    await connection.end();
+
+    res.status(200).send({ ok: true, data: results, total: totalCount });
+  } catch (error) {
+    res.status(400).send({ ok: false, error: error.message });
+  }
+});
+
+// to post data and store in db
+userRouter.post("/", async (req, res) => {
+  const data = req.body.results;
+  try {
+    const connection = await mysql.createConnection(databaseConfig);
+    let batchSize = 50; // insert 50 rows in a single command can  update it to add or less rows in a single batch
     for (let i = 0; i < data.length; i += batchSize) {
       const batch = data.slice(i, i + batchSize);
 
       // placeholders to avoid injection
-      const placeholders = batch.map(() => "(?, ?)").join(", ");
+      const placeholders = batch
+        .map(
+          () =>
+            "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        )
+        .join(", ");
 
       // flatten the data
-      const values = batch.flatMap((item) => [item.name, item.age]);
+      const values = batch.flatMap((item) => {
+        const gender = item.gender;
+        const { title, first, last } = item.name;
+        const { number, name } = item.location.street;
+        const { city, state, country, postcode } = item.location;
+        const { latitude, longitude } = item.location.coordinates;
+        const { offset, description } = item.location.timezone;
+        const email = item.email;
+        const { uuid, username, password, salt, md5, sha1, sha256 } =
+          item.login;
+        const { date, age } = item.dob;
+        const { date: regDate, age: regAge } = item.registered;
+        const phone = item.phone;
+        const cell = item.cell;
+        const { name: idName, value: idValue } = item.id;
+        const { large, medium, thumbnail } = item.picture;
+        const { nat } = item.nat;
+
+        return [
+          gender,
+          title,
+          first,
+          last,
+          number,
+          name,
+          city,
+          state,
+          country,
+          postcode,
+          latitude,
+          longitude,
+          offset,
+          description,
+          uuid,
+          email,
+          username,
+          password,
+          salt,
+          md5,
+          sha1,
+          sha256,
+          date,
+          age,
+          regDate,
+          regAge,
+          phone,
+          cell,
+          idName,
+          idValue,
+          large,
+          medium,
+          thumbnail,
+          nat,
+        ];
+      });
 
       // query for insertion
-      const query = `INSERT INTO users(name, age) VALUES ${placeholders}`;
+
+      const query = `INSERT INTO users (
+        gender, title, first_name, last_name, 
+        street_number, street_name, 
+        city, state, country, postcode, 
+        latitude, longitude, 
+        timezone_offset, timezone_description, uuid, 
+        email, username, password, 
+        salt, md5, sha1, sha256,
+        dob_date, dob_age, 
+        registered_date, registered_age, 
+        phone, cell, 
+        id_name, id_value, 
+        picture_large, picture_medium, picture_thumbnail, 
+        nationality
+      ) VALUES ${placeholders}`;
+
+      // console.log("Number of placeholders:", placeholders.split(",").length);
+      // console.log("Number of values:", values.length);
 
       await connection.query(query, values);
-      connection.end(); //closing the connection
-      res.status(200).send({ msg: "Data added to DB" });
+      await connection.end(); //closing the connection
+      res.status(200).send({ ok: true, msg: "Data added to DB" });
     }
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send({ ok: false, error: error.message });
+  }
+});
+
+userRouter.delete("/", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(databaseConfig);
+    const query = `DELETE FROM users`;
+    await connection.query(query);
+    await connection.end();
+    res.status(200).send({ ok: true, msg: "All rows deleted succesfully" });
+  } catch (error) {
+    res
+      .status(400)
+      .send({ ok: false, msg: "Error while deleting", error: error });
   }
 });
 
